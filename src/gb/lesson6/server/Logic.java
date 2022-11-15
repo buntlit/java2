4e51a26293c24977491c976bb87948644226aade
buntlit <buntlit@mail.ru>
2022-11-15 16:24:51 +0300

    Homework Lesson 6

---
+++ src/gb/lesson6/server/Logic.java
@@ -0,0 +1,50 @@
+package gb.lesson6.server;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Logic {
+    final String END_KEY = "/end";
+    private DataInputStream inSocket;
+    private Scanner inConsole;
+    private DataOutputStream out;
+
+    public Logic(Socket socket) {
+        try {
+            inSocket = new DataInputStream(socket.getInputStream());
+            inConsole = new Scanner(System.in);
+            out = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void getMessage(String name) {
+        new Thread(() -> {
+            try {
+                while (true) {
+                    String strInSocket = inSocket.readUTF();
+                    System.out.println(name + strInSocket);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
+
+    public void sendMessage() {
+          try {
+                while (true) {
+                    String strInConsole = inConsole.nextLine();
+                    if (strInConsole.equals(END_KEY)) {
+                        break;
+                    }
+                    out.writeUTF(strInConsole);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+    }
+}

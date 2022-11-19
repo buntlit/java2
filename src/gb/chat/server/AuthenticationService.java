d7fc5695231ef02dddb7e9fb8c7a9ad297f2750b
buntlit <buntlit@mail.ru>
2022-11-19 20:04:30 +0300

    hw_chat

---
+++ src/gb/chat/server/AuthenticationService.java
@@ -0,0 +1,6 @@
+package gb.chat.server;
+
+public interface AuthenticationService {
+    String getNickByLoginAndPassword(String login, String password);
+    boolean registration(String login, String password, String nick);
+}
